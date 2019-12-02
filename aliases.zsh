6a08d17fe80460d3640300c85dd4ac4f53cbcf7b
Alex Sazonov <a.sazonov@medsolutions.ru>
2019-12-02 12:31:54 +0300

    Adds office deploy

--- aliases.zsh
+++ aliases.zsh
@@ -47,6 +47,7 @@ alias da="docker attach"
 # rails deploy
 # alias cpd="echo -e '\033[0;31m** PRODUCTION DEPLOYMENT **\033[0m';bundle exec cap production deploy"
 alias csd="ssh-add -K; echo -e '\033[0;33m** STAGING DEPLOYMENT **\033[0m';bundle exec cap staging deploy"
+alias cod="ssh-add -K; echo -e '\033[0;33m** OFFICE DEPLOYMENT **\033[0m';bundle exec cap office deploy"
 
 # rails
 alias r="bin/rails s"
